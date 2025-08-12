777d3d34d8500060d9806738f1041562a611a33f
RbMo7 <rbmohit1283@gmail.com>
2025-08-12 18:41:36 +0545

    Edit configurations to check connections beforehand

--- cmd/expense-tracker/main.go
+++ cmd/expense-tracker/main.go
@@ -20,21 +20,18 @@ func main() {
 		return
 	}
 
+	// Check database connection
+	if err := database.CheckConnection(); err != nil {
+		fmt.Printf("Database connection error: %v\n", err)
+		return
+	}
+
 	database.Connect()
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Pong!"))
 	})
 
-
-
-	fmt.Printf("Configuration loaded successfully!\n")
-	fmt.Printf("DB_HOST: %s\n", os.Getenv("DB_HOST"))
-	fmt.Printf("DB_PORT: %s\n", os.Getenv("DB_PORT"))
-	fmt.Printf("DB_USER: %s\n", os.Getenv("DB_USER"))
-	fmt.Printf("DB_PASSWORD: %s\n", os.Getenv("DB_PASSWORD"))
-	fmt.Printf("DB_NAME: %s\n", os.Getenv("DB_NAME"))
-	fmt.Printf("BASE_URL: %s\n", os.Getenv("BASE_URL"))
-
 	http.ListenAndServe(":"+os.Getenv("BASE_PORT"), nil)
+	fmt.Printf("Server is running on port %s\n", os.Getenv("BASE_PORT"))
 }
